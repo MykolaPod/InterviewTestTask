752b7a69cbaabe9314633b92b53be22c9fd37fce
Mykola Pidopryhora <88.general@gmail.com>
2020-03-27 15:38:16 +0200

    add some exception handling

--- PhoneBook/Infrastructure/ExceptionHandling.cs
+++ PhoneBook/Infrastructure/ExceptionHandling.cs
@@ -4,6 +4,7 @@ using System.Net;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
@@ -16,11 +17,26 @@ namespace Infrastructure
             app.Run(async context =>
             {
                 var ex = context.Features.Get<IExceptionHandlerFeature>();
-                //logging errors
+                //logging and handling errors according to business rules
+
                 if (ex?.Error != null)
                 {
                     Console.WriteLine($"Error: {ex.Error.Message}");
+
+                    if (ex.Error is DbUpdateException exception && exception.InnerException != null)
+                    {
+                        var errorMessage = "duplicate key value violates unique constraint";
+                        if (exception.InnerException.Message.Contains(errorMessage))
+                        {
+                            context.Response.ContentType = "application/json";
+                            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                            await context.Response.WriteAsync(JsonConvert.SerializeObject(errorMessage));
+
+                        }
+                    }
                 }
+
+                
 
             });
         }
